dc6285fac574d8bf3e86d256eb43cfc0fc17d3b8
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-04 14:00:11 +0530

    feat: db connection

---
+++ src/common/db.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const MONGO_URI = 'mongodb://localhost:27017/js-api'
+export default async function dbConnect(){
+  try{
+    if(mongoose.connection.readyState === 1){
+      console.log("Already connected");
+      return
+    }
+
+    const connectionString = MONGO_URI
+    if (!connectionString) {
+      throw new Error('MONGODB_URI environment variable is not set')
+    }
+
+    await mongoose.connect(connectionString,{
+      serverSelectionTimeoutMS:3000
+    }).catch(err => {
+      console.log("Error connecting to DB, is it running?")
+    })
+  }catch(error: any){
+    console.error("Something went wrong", error.message);
+  }
+}
+
+dbConnect();
